2cbb4034fa4393c63093e077565761979b32f3c0
weriusska <73849910+weriusska@users.noreply.github.com>
2023-10-04 21:37:10 +0200

    Add files via upload

---
+++ kalendarz/skrypt.js
@@ -0,0 +1,3 @@
+var date = new Date();
+const miesiac = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czewiec", "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"]
+var licz = dzis.getMonth()
